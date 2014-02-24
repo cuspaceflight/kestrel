523f0f264b3614cffffb1ac1d0b09006d7c01e38
DavidWalker13 <survivalist13@gmail.com>
2014-02-24 17:52:10 +0000

    Updated matrix math library to include a a new normalize function that attempts to assign the errors in a more sophisticated manner than the simple normalize function. Also makes use of taylor expansion to avoid square root (how much of a saving is this?). Not sure which normalize function we'll end up using.

--- libraries/MatrixMath/MatrixMath.cpp
+++ libraries/MatrixMath/MatrixMath.cpp
@@ -201,17 +201,41 @@ int MatrixMath::Invert(float* A, int n)
 
 //Vector dot Routine
 // C = A.B
-float MatrixMath::dot(float* A, float* B, int m, int n, int c)
+float MatrixMath::Dot(float* A, float* B, int m, int n, int q int c, int d)
 {
     // A = input matrix/vector (m x n)
-    // B = input vector (m x 1)
+    // B = input vector (m x q)
     // c if column of A to use (1,2 or 3)
+	// d if column of B to use (1,2 or 3)
     // C = output scalar = A.B
+	c = c - 1;
+	d = d - 1;
     float C = 0;
     for (int i=0; i<m;i++) {
-        C = C + A[i*n + (c-1)] * B[i];
+        C = C + A[i*n + c] * B[i*q+d];
     }
     return C;
+}
+
+//Vector cross Routine
+// C = AxB
+void MatrixMath::Cross(float* A, float* B, int n, int c, int d, float* C, int m, int e)
+{
+	// A = input matrix/vector (3 x n)
+	// B = input vector (3 x n)
+	// c is column of A to use (1,2 or 3)
+	// d is column of B to use (1,2 or 3)
+	// C = output matrix (3 * m) = AxB
+	// e is column of C to use (1,2 or 3)
+	c = c - 1;
+	d = d - 1;
+	e = e - 1;
+	float C [3];
+	C[0+e] = A[n+c] * B[2*n+d] - A[2*n+c] * B[n+d];
+	C[m+e] =-A[c] * B[2*n+d] + A[2*n+c] * B[d];
+	C[2*m+e] = A[c] * B[n+d] - A[n+c] * B[d];
+	
+	return C;
 }
 
 //Matrix normalise rows Routine
@@ -238,17 +262,32 @@ void MatrixMath::NormalizeTay3x3(float* A)
 	// A = input matrix (m x n)
 
 	//an implementation which avoids square roots is below but in matlab code
-	/*xy_error=dot(R2(:,1),R2(:,2));
-	R3=zeros(3,3);
-	R3(:,1) = R2(:,1) - 0.5*xy_error*R2(:,2);
-	R3(:,2) = R2(:,2) - 0.5*xy_error*R2(:,1);
-	R3(:,3) = cross(R3(:,1),R3(:,2));
-	%make magnitudes equal to one, as the difference will be small can use
-	%taylor expansion to avoid square root
-	R3(:,1) = 0.5*(3 - dot(R3(:,1),R3(:,1))) * R3(:,1);
-	R3(:,2) = 0.5*(3 - dot(R3(:,2),R3(:,2))) * R3(:,2);
-	R3(:,3) = 0.5*(3 - dot(R3(:,3),R3(:,3))) * R3(:,3);
-	*/
-	
 
+	float xy_error=0;
+	for (int i = 0; i <= 2; i++){ //cycle through rows
+		xy_error = xy_error + A[3 * i] * A[1 + 3 * i];  //Matrix.dot(R2(:, 1), R2(:, 2));
+	}
+	float A2[3][3];
+	for (int i = 0; i <= 2; i++){
+		A2[i][0] = A[i][0] - 0.5*xy_error*A[i][1];
+		A2[i][1] = A[i][1] - 0.5*xy_error*A[i][0];
+	}
+
+	Matrix.Cross(A, A, 3, 1, 2, A2, 3, 3);
+
+	//R3(:,1) = R2(:,1) - 0.5*xy_error*R2(:,2);
+	//R3(:,2) = R2(:,2) - 0.5*xy_error*R2(:,1);
+	//R3(:,3) = cross(R3(:,1),R3(:,2));
+	//make magnitudes equal to one, as the difference will be small can use
+	//taylor expansion to avoid square root
+	for (int j = 0; j <= 2; j++){ //cycle through columns
+		float dot = Matrix.Dot(A2, A2, 3, 3, 3, (j + 1), (j + 1) );
+		for (int i = 0; i <= 2; i++){ //cycle through rows
+			A2[i][j] = 0.5*(3 - A2[i][j] * dot);
+		}
+	
+	}
+	//R3(:,1) = 0.5*(3 - dot(R3(:,1),R3(:,1))) * R3(:,1);
+	//R3(:,2) = 0.5*(3 - dot(R3(:,2),R3(:,2))) * R3(:,2);
+	//R3(:,3) = 0.5*(3 - dot(R3(:,3),R3(:,3))) * R3(:,3);
 }
